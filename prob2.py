1d661b5ce925e36be76c575fc84e6f45abfffb2c
A≡Azryel <mmsantos@carc.pshs.edu.ph>
2026-09-22 18:37:35 +0800

    Update username validation logic and valid characters

--- prob2.py
+++ prob2.py
@@ -2,9 +2,9 @@
 # Mihr Azyel M. Santos, Kalel Gabriel F. Dizon, Pollux C. Rivera
 
 #List of valid characters for password
-valid_characters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+valid_characters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","_"]
 #User enters password
-password = input("Enter your username: ")
+password = input("Enter your username (alpha-numeric and underscore. "_"): ")
 #Counts the amount of letters
 letter = len(password)
 #Checks the conditions here
